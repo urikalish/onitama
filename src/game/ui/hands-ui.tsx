04f77c13ec76b328f6a2e390048c02378d97ce19
kalish <uri.kalish@gmail.com>
2024-02-27 23:45:58 +0200

    wip

--- src/game/ui/hands-ui.tsx
+++ src/game/ui/hands-ui.tsx
@@ -1,7 +1,7 @@
 import './hands.css';
 
 import { Box } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { Position } from '../model/position';
 
@@ -15,22 +15,71 @@ type HandsUIProps = {
 
 export function HandsUi({ p }: HandsUIProps) {
     const [cards, setCards] = useState<string[][]>([]);
+    const [selected, setSelected] = useState<string>('');
+
+    function isSelectable(playerIndex: number): boolean {
+        return playerIndex === p.armyIndex;
+    }
 
     useEffect(() => {
         setCards([p.handsData[0].split(','), p.handsData[1].split(',')]);
     }, [p.handsData]);
 
+    const handleCardClick = useCallback((event: any) => {
+        setSelected(event.target.dataset.name);
+    }, []);
+
     return (
         <Box className="hands">
             <Box className="hand hand--blue">
-                {cards[0] && <Box className={`card card--blue ${p.armyIndex === 0 ? 'card--usable' : ''}`}>{fixName(cards[0][0])}</Box>}
-                {cards[0] && <Box className={`card card--blue ${p.armyIndex === 0 ? 'card--usable' : ''}`}>{fixName(cards[0][1])}</Box>}
-                {cards[0] && cards[0][2] && <Box className="card card--blue card--waiting">{fixName(cards[0][2])}</Box>}
+                {cards[0] && (
+                    <Box
+                        data-name={cards[0][0]}
+                        className={`card card--blue ${isSelectable(0) ? 'card--selectable' : ''} ${selected === cards[0][0] ? 'card--selected' : ''}`}
+                        onClick={handleCardClick}
+                    >
+                        {fixName(cards[0][0])}
+                    </Box>
+                )}
+                {cards[0] && (
+                    <Box
+                        data-name={cards[0][1]}
+                        className={`card card--blue ${isSelectable(0) ? 'card--selectable' : ''} ${selected === cards[0][1] ? 'card--selected' : ''}`}
+                        onClick={handleCardClick}
+                    >
+                        {fixName(cards[0][1])}
+                    </Box>
+                )}
+                {cards[0] && cards[0][2] && (
+                    <Box data-name={cards[0][2]} className="card card--blue">
+                        {fixName(cards[0][2])}
+                    </Box>
+                )}
             </Box>
             <Box className="hand hand--red">
-                {cards[1] && <Box className={`card card--red ${p.armyIndex === 1 ? 'card--usable' : ''}`}>{fixName(cards[1][0])}</Box>}
-                {cards[1] && <Box className={`card card--red ${p.armyIndex === 1 ? 'card--usable' : ''}`}>{fixName(cards[1][1])}</Box>}
-                {cards[1] && cards[1][2] && <Box className="card card--red card--waiting">{fixName(cards[1][2])}</Box>}
+                {cards[1] && (
+                    <Box
+                        data-name={cards[1][0]}
+                        className={`card card--red ${isSelectable(1) ? 'card--selectable' : ''} ${selected === cards[1][0] ? 'card--selected' : ''}`}
+                        onClick={handleCardClick}
+                    >
+                        {fixName(cards[1][0])}
+                    </Box>
+                )}
+                {cards[1] && (
+                    <Box
+                        data-name={cards[1][1]}
+                        className={`card card--red ${isSelectable(1) ? 'card--selectable' : ''} ${selected === cards[1][1] ? 'card--selected' : ''}`}
+                        onClick={handleCardClick}
+                    >
+                        {fixName(cards[1][1])}
+                    </Box>
+                )}
+                {cards[1] && cards[1][2] && (
+                    <Box data-name={cards[1][2]} className="card card--red">
+                        {fixName(cards[1][2])}
+                    </Box>
+                )}
             </Box>
         </Box>
     );
