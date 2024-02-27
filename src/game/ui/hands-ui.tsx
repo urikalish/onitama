d4718e8f4312beb76dd1623d7ac8dc99e75032aa
kalish <uri.kalish@gmail.com>
2024-02-27 20:41:56 +0200

    wip

--- src/game/ui/hands-ui.tsx
+++ src/game/ui/hands-ui.tsx
@@ -3,36 +3,35 @@ import './hands.css';
 import { Box } from '@mui/material';
 import { useEffect, useState } from 'react';
 
+import { Position } from '../model/position';
+
 function fixName(name: string) {
     return name.replaceAll('_', ' ');
 }
 
 type HandsUIProps = {
-    handsData: string[];
-    activePlayerIndex: number;
+    p: Position;
 };
 
-export function HandsUi({ handsData, activePlayerIndex }: HandsUIProps) {
+export function HandsUi({ p }: HandsUIProps) {
     const [cards, setCards] = useState<string[][]>([]);
 
     useEffect(() => {
-        setCards([handsData[0].split(','), handsData[1].split(',')]);
-    }, [handsData]);
+        setCards([p.handsData[0].split(','), p.handsData[1].split(',')]);
+    }, [p.handsData]);
 
     return (
-        handsData && (
-            <Box className="hands">
-                <Box className="hand hand--blue">
-                    {cards[0] && <Box className={`card card--blue ${activePlayerIndex === 0 ? 'card--usable' : ''}`}>{fixName(cards[0][0])}</Box>}
-                    {cards[0] && <Box className={`card card--blue ${activePlayerIndex === 0 ? 'card--usable' : ''}`}>{fixName(cards[0][1])}</Box>}
-                    {cards[0] && cards[0][2] && <Box className="card card--blue card--waiting">{fixName(cards[0][2])}</Box>}
-                </Box>
-                <Box className="hand hand--red">
-                    {cards[1] && <Box className={`card card--red ${activePlayerIndex === 1 ? 'card--usable' : ''}`}>{fixName(cards[1][0])}</Box>}
-                    {cards[1] && <Box className={`card card--red ${activePlayerIndex === 1 ? 'card--usable' : ''}`}>{fixName(cards[1][1])}</Box>}
-                    {cards[1] && cards[1][2] && <Box className="card card--red card--waiting">{fixName(cards[1][2])}</Box>}
-                </Box>
+        <Box className="hands">
+            <Box className="hand hand--blue">
+                {cards[0] && <Box className={`card card--blue ${p.armyIndex === 0 ? 'card--usable' : ''}`}>{fixName(cards[0][0])}</Box>}
+                {cards[0] && <Box className={`card card--blue ${p.armyIndex === 0 ? 'card--usable' : ''}`}>{fixName(cards[0][1])}</Box>}
+                {cards[0] && cards[0][2] && <Box className="card card--blue card--waiting">{fixName(cards[0][2])}</Box>}
             </Box>
-        )
+            <Box className="hand hand--red">
+                {cards[1] && <Box className={`card card--red ${p.armyIndex === 1 ? 'card--usable' : ''}`}>{fixName(cards[1][0])}</Box>}
+                {cards[1] && <Box className={`card card--red ${p.armyIndex === 1 ? 'card--usable' : ''}`}>{fixName(cards[1][1])}</Box>}
+                {cards[1] && cards[1][2] && <Box className="card card--red card--waiting">{fixName(cards[1][2])}</Box>}
+            </Box>
+        </Box>
     );
 }
