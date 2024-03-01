621e3bce31dbfd7e2e7d043378f8a06ce2ccfdaf
kalish <uri.kalish@gmail.com>
2024-03-02 01:49:20 +0200

    wip

--- src/game/ui/card-ui.tsx
+++ src/game/ui/card-ui.tsx
@@ -1,7 +1,7 @@
 import './card.css';
 
 import { Box } from '@mui/material';
-import { useCallback, useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 
 import { getCardMoves } from '../model/card';
 
@@ -59,6 +59,7 @@ export function CardUI({ name, armyIndex, isSelectable, isSelected, onSelectCard
 
     return (
         <Box data-name={name} className={`card ${armyIndex === 0 ? 'blue' : 'red'} ${isSelectable ? 'selectable' : ''} ${isSelected ? 'selected' : ''}`} onClick={handleSelectCard}>
+            <Box className="cover card-cover" />
             <Box className="card-name">{fixName(name)}</Box>
             <Box ref={cardGridRef} className="card-grid" />
         </Box>
