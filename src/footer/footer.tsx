985a16569d48a99305a0660cf52dca8d7b224827
kalish <uri.kalish@gmail.com>
2024-03-08 00:44:12 +0200

    wip

--- src/footer/footer.tsx
+++ src/footer/footer.tsx
@@ -3,5 +3,5 @@ import './footer.css';
 import { Typography } from '@mui/material';
 
 export function Footer() {
-    return <Typography className="footer">by Uri Kalish</Typography>;
+    return <Typography className="footer">Developed by Uri Kalish</Typography>;
 }
