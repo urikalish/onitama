add25628b2fe82359ad1cc08373020bfc7bfba28
Uri Kalish <urikalish@users.noreply.github.com>
2024-03-09 17:00:56 +0200

    Update home.tsx

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -48,7 +48,7 @@ export function Home() {
             <Box sx={{ position: 'absolute', inset: '2rem' }}>
                 <Typography variant="h4">About</Typography>
                 <Typography variant="body2" sx={{ color: 'var(--color--gray--light)', marginTop: '0.5rem' }}>
-                    Onitama is a two-player, perfect information abstract game created in 2014 by game designer Shimpei Sato. This online version was developed by Uri Kalish in
+                    Onitama is a two-player, perfect information abstract game created in 2014 by game designer Shimpei Sato. This digital version was developed by Uri Kalish in
                     2024.
                 </Typography>
                 <Typography variant="h4" sx={{ marginTop: '2rem' }}>
