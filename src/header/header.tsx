92751ae411a1420316c789728a7d983db7e0a79d
kalish <uri.kalish@gmail.com>
2024-03-10 01:00:40 +0200

    wip

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -4,14 +4,14 @@ import { AppBar, Toolbar } from '@mui/material';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import onitamaLogoTextImage from './onitama-logo-text.png';
+import appLogoImage from './onitama-logo-text.png';
 
 export function Header() {
     return (
         <AppBar color="primary" position="static" className="header--app-bar">
             <Toolbar variant="dense" className="header--toolbar">
                 <NavLink to="/" className="flex">
-                    <img src={onitamaLogoTextImage} alt="Onitama" className="header--logo-image" />
+                    <img src={appLogoImage} alt="Onitama" className="header--logo-image" />
                 </NavLink>
             </Toolbar>
         </AppBar>
