ca0c46d80bc5427bbef20e0e585870695d4bb682
kalish <uri.kalish@gmail.com>
2024-03-11 09:44:41 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import comlink from 'vite-plugin-comlink';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [comlink(), react()],
     server: {
         port: 2014,
+    },
+    worker: {
+        plugins: () => [comlink()],
     },
 });
