648f02ba1062c5ff50276d7484c4fd3da017aee8
kalish <uri.kalish@gmail.com>
2024-03-11 22:56:13 +0200

    bot

--- src/start/start.tsx
+++ src/start/start.tsx
@@ -1,25 +1,33 @@
-import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, Radio, RadioGroup, Typography } from '@mui/material';
+import './start.css';
+
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, Radio, RadioGroup, Slider, Typography } from '@mui/material';
 import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { getRandomCardsNames } from '../game/model/card';
 
-const LOCAL_STORAGE_KEY_PLAYER_TYPES = 'playerTypes';
+const LOCAL_STORAGE_KEY_OPPONENT_TYPE = 'opponentType';
+const LOCAL_STORAGE_KEY_BOT_STRENGTH = 'botStrength';
 const LOCAL_STORAGE_KEY_DECK_NAMES = 'deckNames';
 
 export function Start() {
-    const [playerTypes, setPLayerTypes] = useState('human,human');
+    const [opponentType, setOpponentType] = useState('human');
+    const [botStrength, setBotStrength] = useState(0);
     const [baseDeck, setBaseDeck] = useState(true);
-    const [pathDeck, setPathDeck] = useState(true);
-    const [windAndPromoDecks, setWindAndPromoDecks] = useState(true);
+    const [pathDeck, setPathDeck] = useState(false);
+    const [windAndPromoDecks, setWindAndPromoDecks] = useState(false);
     const [canSubmit, setCanSubmit] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
         let value;
-        value = localStorage.getItem(LOCAL_STORAGE_KEY_PLAYER_TYPES);
+        value = localStorage.getItem(LOCAL_STORAGE_KEY_OPPONENT_TYPE);
         if (value) {
-            setPLayerTypes(value);
+            setOpponentType(value);
+        }
+        value = localStorage.getItem(LOCAL_STORAGE_KEY_BOT_STRENGTH);
+        if (value) {
+            setBotStrength(Number(value));
         }
         value = localStorage.getItem(LOCAL_STORAGE_KEY_DECK_NAMES);
         if (value) {
@@ -30,8 +38,12 @@ export function Start() {
         }
     }, []);
 
-    const handleChangePlayerTypes = useCallback((event: any) => {
-        setPLayerTypes(event.target.value);
+    const handleChangeOpponentType = useCallback((event: any) => {
+        setOpponentType(event.target.value);
+    }, []);
+
+    const handleChangeBotStrength = useCallback((event: any) => {
+        setBotStrength(event.target.value);
     }, []);
 
     useEffect(() => {
@@ -66,28 +78,49 @@ export function Start() {
             decks.push('wind');
             decks.push('promo');
         }
-        localStorage.setItem(LOCAL_STORAGE_KEY_PLAYER_TYPES, playerTypes);
-        localStorage.setItem(LOCAL_STORAGE_KEY_DECK_NAMES, decks.join(','));
         const cardNames = getRandomCardsNames(decks, 5);
-        navigate(`/game?players=${playerTypes}&cards=${cardNames.join(',')}`);
-    }, [playerTypes, baseDeck, pathDeck, windAndPromoDecks]);
+        localStorage.setItem(LOCAL_STORAGE_KEY_OPPONENT_TYPE, opponentType);
+        localStorage.setItem(LOCAL_STORAGE_KEY_BOT_STRENGTH, botStrength.toString());
+        localStorage.setItem(LOCAL_STORAGE_KEY_DECK_NAMES, decks.join(','));
+        navigate(`/game?players=human,${opponentType}&cards=${cardNames.join(',')}`);
+    }, [opponentType, baseDeck, pathDeck, windAndPromoDecks]);
 
     return (
         <Box className="start page">
             <Box className="page-cover" />
             <Box className="page--content-and-actions">
                 <Box className="page--content">
-                    <Typography variant="h4">Players</Typography>
-                    <FormControl>
-                        <RadioGroup value={playerTypes} onChange={handleChangePlayerTypes} name="players-radio-buttons-group">
-                            <FormControlLabel value="human,human" control={<Radio />} label="Human vs. human" />
-                            <FormControlLabel value="human,bot" control={<Radio />} label="Human vs. bot" />
-                        </RadioGroup>
-                    </FormControl>
-                    <Typography variant="h4" sx={{ marginTop: '2rem' }}>
-                        Movement
-                    </Typography>
                     <FormGroup>
+                        <Typography variant="h4">Opponent</Typography>
+                        <FormControl>
+                            <RadioGroup value={opponentType} onChange={handleChangeOpponentType} name="opponent-type-radio-group">
+                                <FormControlLabel value="human" control={<Radio />} label="Human" />
+                                <FormControlLabel value="bot" control={<Radio />} label="Bot" />
+                            </RadioGroup>
+                        </FormControl>
+                        <Box className={`start--bot-level ${opponentType === 'bot' ? '' : 'hidden'}`}>
+                            <Typography variant="body2" className="start--bot-level-slider-label">
+                                Easy
+                            </Typography>
+                            <Slider
+                                value={botStrength}
+                                step={1}
+                                marks
+                                min={0}
+                                max={6}
+                                disabled={opponentType !== 'bot'}
+                                onChange={handleChangeBotStrength}
+                                className="start--bot-level-slider"
+                            />
+                            <Typography variant="body2" className="start--bot-level-slider-label">
+                                Hard
+                            </Typography>
+                        </Box>
+                    </FormGroup>
+                    <FormGroup>
+                        <Typography variant="h4" sx={{ marginTop: '2rem' }}>
+                            Movement
+                        </Typography>
                         <FormControlLabel control={<Checkbox checked={baseDeck} onChange={handleChangeBaseDeck} />} label="Base deck" />
                         <FormControlLabel control={<Checkbox checked={pathDeck} onChange={handleChangePathDeck} />} label="Sensei's Path cards" />
                         <FormControlLabel control={<Checkbox checked={windAndPromoDecks} onChange={handleChangeWindAndPromoDecks} />} label="Way of the Wind + promo cards" />
