592f8299bd470184434d223d862b352d2b499105
kalish <uri.kalish@gmail.com>
2024-03-17 23:22:26 +0200

    wip

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -5,14 +5,13 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { Music } from '../music/music';
-import appLogoImage from './onitama-logo-text.png';
 
 export function Header() {
     return (
         <AppBar color="primary" position="static" className="header--app-bar">
             <Toolbar variant="dense" className="header--toolbar">
                 <NavLink to="/" className="flex">
-                    <img alt="Onitama" className="header--logo-image" src={appLogoImage} />
+                    <img alt="Onitama" className="header--logo-image" src="/img/logo/onitama-text.png" />
                 </NavLink>
                 <Music />
             </Toolbar>
