4a017bfd315cb119b6a7491bdbc581479b66dd50
kalish <uri.kalish@gmail.com>
2024-03-30 13:22:20 +0300

    wip

--- src/services/utils.ts
+++ src/services/utils.ts
@@ -18,3 +18,13 @@ export function getRandomNumber(numOfDigits: number) {
     const max = Math.pow(10, numOfDigits) - 1;
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function getDateTime(date: Date) {
+    const year = date.getFullYear().toString().slice(2); // Get last two digits of the year
+    const month = ('0' + (date.getMonth() + 1)).slice(-2); // Format month as two digits
+    const day = ('0' + date.getDate()).slice(-2); // Format day as two digits
+    const hours = ('0' + date.getHours()).slice(-2); // Format hours as two digits
+    const minutes = ('0' + date.getMinutes()).slice(-2); // Format minutes as two digits
+    const seconds = ('0' + date.getSeconds()).slice(-2); // Format seconds as two digits
+    return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+}
